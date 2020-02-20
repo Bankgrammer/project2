ffda613adccbab71f52777b2178246b8d1f86295
Thabang Moeketsi <codeconstable@gmail.com>
2020-02-20 17:09:32 +0200

    Intial Project

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $('.nav-button').click(function () {
+    $('.nav-button').toggleClass('change');
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  });
+});
+
+ // STATS
+
+ $(function () {
+
+    $(".counter").counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+}); 
+
+
+
+
+
+
+
+
+
+
+
